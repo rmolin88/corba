060fe977190b9fdd9f1155e2b6ba7da723c3b01d
Reinaldo Molina <rmolin88@gmail.com>
2018-07-13 21:55:20 -0400

    Working initial commit

---
+++ tests/test_corba_client.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <rapidjson/document.h>
+#include <rapidjson/error/en.h>
+
+#include "account.hh"
+
+TEST(Config, GoodConfigFile) {
+	// int argc = 1;
+	// char *cmd_opts[] =
+	// {
+		// "-c /home/reinaldo/Documents/ML_SC2/Arrancar0/test/config/config.json",
+	// };
+
+	// config &cfg = config::get_config();
+
+	// int ret = cfg.parse_config_file(1, cmd_opts);
+
+	EXPECT_EQ(1,1);
+}
